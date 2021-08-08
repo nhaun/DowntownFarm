48079d69d2b6fe5eb0162ba6aeb4872fc76d6479
Niklas <nik.haun@gmx.de>
2021-08-08 22:36:59 +0200

    Last refinements

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -4,6 +4,7 @@ function hideLoader() {
 }
 
 //$(window).ready(hideLoader);
+window.scrollTo(0,0);
 
 // Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
 setTimeout(hideLoader, 2500);
